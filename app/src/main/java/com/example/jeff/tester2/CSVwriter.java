066a5b11dc08cc24afa53d4ed87112fe824e7f70
Jeff <Jeff_schonewille@ziggo.nl>
2014-10-02 10:05:03 +0200

    changed the writer to work on different phones (one x )

--- app/src/main/java/com/example/jeff/tester2/CSVwriter.java
+++ app/src/main/java/com/example/jeff/tester2/CSVwriter.java
@@ -1,69 +1,72 @@
 package com.example.jeff.tester2;
 
 
-import java.io.BufferedWriter;
+import android.os.Environment;
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import android.os.Environment;
-import android.text.format.DateFormat;
+import java.util.Date;
 
 /**
  * Created by Jeff on 16-9-2014.
  */
 public class CSVwriter {
-
-
-    File folder = new File(Environment.getExternalStorageDirectory() + "/Android/data/com.example.jeff.tester2");
     boolean success = true;
-    public boolean writing =  false;
+    public boolean writing = false;
     FileWriter writer;
     File gpxfile;
 
 
-    public CSVwriter(){
+    public CSVwriter() {
+
         try {
-
-            if (!folder.exists()) {
-                success = folder.mkdir();
-            }
-            if (success) {
-                String CSVfilename = (DateFormat.format("dd-MM-yyyy hh:mm:ss", new java.util.Date()).toString());
-                CSVfilename = CSVfilename.replace(" ","-");
-                CSVfilename = "iBeaconTest" + CSVfilename;
-                String name = CSVfilename + ".csv";
-                gpxfile = new File(folder, name);
-                writer = new FileWriter(gpxfile);
-
-            } else {
-                System.out.println("Error making writer");
-            }
-        } catch (IOException e) {
-            e.printStackTrace();
-
+            writer = new FileWriter(getOutputfile());
+        } catch (Exception e) {
+            System.out.println("henk");
         }
     }
+
+    private File getOutputfile() {
+        File output = new File(Environment.getExternalStorageDirectory()
+                + "/Android/data/"
+                + "/com.example.jeff.tester2");
+
+        // create storage folder if it does not exist
+        if (!output.exists()) {
+            if (!output.mkdirs()) {
+                return null;
+            }
+        }
+
+        String filename = new SimpleDateFormat("ddMMyyyy_HHmm").format(new Date());
+        filename = "iBeaconTest" + filename;
+        filename = filename + ".csv";
+        File outfile;
+        outfile = new File((output.getPath() + File.separator + filename));
+        return outfile;
+    }
+
 
     public void writeCsvRow(ArrayList<String> arrayList) throws IOException {
         writing = true;
         String test = "";
         for (String item : arrayList) {
             test += item;
-            test +=" ,";
+            test += " ,";
         }
-        test +="\n";
+        test += "\n";
 
         writer.write(test);
     }
 
     public void saveFile() throws IOException {
-        if (writing)
-        {
+        if (writing) {
             writer.flush();
             writer.close();
             writing = false;
         }
     }
-
 }
